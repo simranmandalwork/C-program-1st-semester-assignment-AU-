7483f6ca8af9fb10b17cf453d64081ca90050b90
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-08-12 20:51:27 +0530

    Add files via upload

---
+++ Day 1/Substract two number taking from the user.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,result;
+	printf("Enter the first number:");
+	scanf("%d",&a);
+	printf("Enter the second number:");
+	scanf("%d",&b);
+	result=a-b;
+	printf("result=%d\n",result);
+	return 0;
+}
