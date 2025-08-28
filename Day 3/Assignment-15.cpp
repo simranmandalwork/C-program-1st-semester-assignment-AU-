dc73a6ba181e4f19e382f0c6586a8c0a9d74354e
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-08-29 02:06:13 +0530

    Assignment-15.cpp

--- Day 3/Assignment-15.cpp
+++ Day 3/Assignment-15.cpp
@@ -1,38 +1,17 @@
 #include <stdio.h>
 #include <math.h>
-
 int main() {
-    float a, b, c, d, root1, root2, realPart, imagPart;
-
-    printf("Enter coefficient a:");
+    float a, b, c, d, root1, root2;
+    printf("\nEnter the coefficient of a: ");
     scanf("%f", &a);
-    printf("Enter coefficient b:");
+    printf("\nEnter the coefficient of b: ");
     scanf("%f", &b);
-    printf("Enter coefficient c:");
+    printf("\nEnter the coefficient of c: ");
     scanf("%f", &c);
-    d = b * b - 4 * a * c;
-    if (d > 0)
-	{
-        root1 = (-b+sqrt(d))/(2*a);
-        root2 = (-b-sqrt(d))/(2*a);
-        printf("Roots are real and different.\n");
-        printf("Root 1 = %.2f\n", root1);
-        printf("Root 2 = %.2f\n", root2);
-    } 
-    else if (d==0)
-	{
-        root1 = -b / (2 * a);
-        printf("Roots are real and equal.\n");
-        printf("Root 1 = Root 2 = %.2f\n", root1);
-    } 
-    else 
-	{
-        realPart = -b / (2 * a);
-        imagPart = sqrt(-d) / (2 * a);
-        printf("Roots are complex and different.\n");
-        printf("Root 1 = %.2f + %.2fi\n", realPart, imagPart);
-        printf("Root 2 = %.2f - %.2fi\n", realPart, imagPart);
-    }
-
+    d=b*b-4*a*c;
+    root1=(-b+sqrt(d))/(2*a);
+    root2=(-b-sqrt(d))/(2*a);
+    printf("\nRoot1 of a quadratic equation is: %.2f", root1);
+    printf("\nRoot2 of a quadratic equation is: %.2f", root2);
     return 0;
 }
