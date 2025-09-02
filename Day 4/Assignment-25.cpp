2d79a5f3770903de7b067eae8bae9362fe4f87eb
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-09-03 00:31:09 +0530

    Add files via upload

---
+++ Day 4/Assignment-25.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter 1st number:");
+    scanf("%d",&a);
+    printf("\nEnter 2nd number:");
+    scanf("%d",&b);
+    printf("\nEnter 3rd number:");
+    scanf("%d",&c);
+    if(a>b)
+	{
+    if (a>c)
+	{
+    printf("\n%d is the greatest.",a);
+    }
+	else
+	{
+    printf("\n%d is the greatest.",c);
+    }
+    }
+	else
+	{
+    if(b>c)
+	{
+    printf("\n%d is the greatest.",b);
+    }
+	else
+	{
+    printf("\n%d is the greatest.",c);
+    }
+    }
+    return 0;
+}
