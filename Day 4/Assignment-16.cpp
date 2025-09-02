e2ca21699fd49b5de2a2947789bfa13175af7702
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-09-03 00:25:14 +0530

    Assignment-16.cpp

--- Day 4/Assignment-16.cpp
+++ Day 4/Assignment-16.cpp
@@ -3,14 +3,15 @@ int main()
  {
     int roll;
     printf("Enter Roll Number:");
-    scanf("%d", &roll);
-    if (roll == 100) 
+    scanf("%d",&roll);
+    if(roll==100) 
 	{
     printf("\nRoll 100 is present.");
     }
-    if (roll != 100) 
+    if(roll!=100) 
 	{
     printf("\nRoll 100 is NOT present.");
     }
     return 0;
 }
+
