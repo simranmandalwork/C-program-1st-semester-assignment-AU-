fc3a199db58c3f05f1453d6d6c6de7a5661fc5e8
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-10-28 15:07:47 +0530

    Add files via upload

---
+++ Day-10/Assignment-50.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,a,n1,r=0;
+	printf("Enter a digit :");
+	scanf("%d",&n);
+	n1=n;
+	while(n>0)
+	{
+		a=n%10;
+		r=r*10+a;
+		n=n/10;
+	}
+if(r==n1)
+{
+printf("\n %d number is a palindrom number ",r);
+}
+else
+printf("\n %d is not a palindrom number",r);
+	return 0;
+}
