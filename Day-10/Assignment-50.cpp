ba9aa43eac35268986ddcd672846322ace471345
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-10-28 16:19:50 +0530

    Fix palindrome number check and update prompt

--- Day-10/Assignment-50.cpp
+++ Day-10/Assignment-50.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	int n,a,n1,r=0;
-	printf("Enter a digit :");
+	printf("Enter a number :");
 	scanf("%d",&n);
 	n1=n;
 	while(n>0)
@@ -18,4 +18,5 @@ printf("\n %d number is a palindrom number ",r);
 else
 printf("\n %d is not a palindrom number",r);
 	return 0;
+
 }
