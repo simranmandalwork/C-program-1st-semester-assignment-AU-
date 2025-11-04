de35aa86173ba05cfdbf07873ae12b5666d27ae6
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-11-04 20:55:32 +0530

    Add files via upload

---
+++ Day 11/Assignment-61.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],transpose[10][10],rows,cols,i,j;
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+    printf("Enter number of columns: ");
+    scanf("%d",&cols);
+    printf("Enter elements of the matrix:\n");
+    for(i=0;i<rows;i++)
+	{
+        for(j=0;j<cols;j++)
+		{
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<rows;i++)
+	{
+        for(j=0;j<cols;j++)
+		{
+            transpose[j][i]=a[i][j];
+        }
+    }
+    printf("\nOriginal Matrix:\n");
+    for(i=0;i<rows;i++)
+	{
+        for(j=0;j<cols;j++)
+		{
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nTranspose of the Matrix:\n");
+    for(i=0;i<cols;i++)
+	{
+        for(j=0;j<rows;j++)
+		{
+            printf("%d ",transpose[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
