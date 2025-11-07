cb5e187e55216e723a00fc5cf38faf24e58df972
Simran mandal <225925718+simranmandalwork@users.noreply.github.com>
2025-11-07 17:41:33 +0530

    Add files via upload

---
+++ Day 11/Assigment-62.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    int i,j,a,b=0;
+    printf("Enter a string: ");
+    scanf("%s",str);
+    a=strlen(str);
+    j=a-1;
+    for(i=0;i<a;i++)
+    {
+        if(str[i]!=str[j])
+        {
+        b=1;
+        break;
+        }
+        j--;
+    }
+    if(b==0)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+    return 0;
+}
